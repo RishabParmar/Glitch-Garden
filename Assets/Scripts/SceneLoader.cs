072b6fac844751bac296e9c2e28210931cd8af1b
Rishab Parmar <parmar.rishab@gmail.com>
2020-08-28 01:24:37 +0530

    Added Splash, Start and Game scenes, scaled the gameplay area in the co-ordinate system

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    private void Start()
+    {
+        // Run the following method only for splash screen to start screen movement
+        LoadNextSceneAfterDelay();
+    }
+
+    public void LoadNextScene()
+    {
+
+    }
+
+    public void LoadNextSceneAfterDelay()
+    {
+        if(SceneManager.GetActiveScene().buildIndex ==0)
+        {
+            StartCoroutine(LoadingNextScene(SceneManager.GetActiveScene().buildIndex));
+        }
+    }
+
+    private IEnumerator LoadingNextScene(int currentSceneId)
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(currentSceneId + 1);
+    }
+}
