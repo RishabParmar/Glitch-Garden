10d9cc8c5ea9f90129a9681b2b3700303984b13d
Rishab Parmar <parmar.rishab@gmail.com>
2020-08-29 00:36:53 +0530

    Added animations to the trophy and lizard object and translated the lizard

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [SerializeField] float walkSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * walkSpeed);
+    }
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -14,7 +14,7 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadNextScene()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void LoadNextSceneAfterDelay()
